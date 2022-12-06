32bd742e67d8d35d081204b5256903a18c3c2b53
朱竹林 <741645596@qq.com>
2022-12-06 17:31:54 +0800

    1

---
+++ Assets/Script/Majiang/MJAction.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MJAction : MonoBehaviour
+{
+    /// <summary>
+    /// 正面材质
+    /// </summary>
+    public Material fontMaterial;
+    /// <summary>
+    /// 背面材质
+    /// </summary>
+    public Material backMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// 设定麻将的位置
+    /// </summary>
+    /// <param name="pos"></param>
+    public void SetPos(Vector3 pos)
+    { 
+    }
+    /// <summary>
+    /// 设定麻将的旋转
+    /// </summary>
+    public void SetDir()
+    {
+        
+    }
+    /// <summary>
+    /// 是否显示影子
+    /// </summary>
+    /// <param name="isShow"></param>
+    public void ShowShadow(bool isShow)
+    {
+        
+    }
+    /// <summary>
+    /// 设置麻将牌面
+    /// </summary>
+    public void SetMajiangNum()
+    {
+        
+    }
+}
