619b14c2e4eabc7e14ac1d3168a3889a772e26b8
741645596 <741645596@qq.com>
2023-05-15 16:19:49 +0800

    1

--- Assets/Art/Script/Core/Helper/ObjectUtil.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-public class ObjectUtil
-{
-    public static string GetHashCodeString(object obj)
-    {
-        return RuntimeHelpers.GetHashCode(obj).ToString("X8");
-    }
-}
--- Assets/Art/Script/Core/Utils/CommonEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Weile.Core.Utils
-{
-
-
-
-}
-
-
